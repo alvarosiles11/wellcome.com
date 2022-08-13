99394c8cf1c145719ddd2d74118ff006a9fa7df6
Alvaro Siles Estrada <alvarosiles1@gmail.com>
2022-08-13 07:08:14 -0400

    👾

---
+++ scroll.js
@@ -0,0 +1,19 @@
+var mybutton = document.getElementById("scrollTop");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+mybutton.addEventListener("click", function () {
+  document.body.scrollIntoView({
+    behavior: "smooth",
+  });
+});
